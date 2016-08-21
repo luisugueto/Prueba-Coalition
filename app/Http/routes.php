57f549d5826f7ba974ea579f9f179367d9f99956
Luis Ugueto <blink242@outlook.com>
2016-08-20 22:56:25 -0430

    Create View, Routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,15 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+use App\Product;
+use Illuminate\Http\Request;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/**
+ *  Get the full list of products
+ */
+Route::get('/','ProductController@index');
+Route::resource('producto', 'ProductController');
+
+/**
+ *  Get a specific product
+ */
+
---
+++ resources/views/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="csrf-token" id="token" content="{{ csrf_token() }}">
+	<title>Coalition Technologies</title>
+    <link href="bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="bootstrap/dist/css/bootstrap-theme.min.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 text-center">
+				<h1>Coalition Test Product Manager</h1>
+			</div>
+			<body>
+<script src="js/jquery.min.js"></script>
+<script src="bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="js/ajax.js"></script>
+</html>
